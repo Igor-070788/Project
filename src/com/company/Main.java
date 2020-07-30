bb6e43acd4ad584d92e05c88b39adf7819600791
Igor-070788 <68988262+Igor-070788@users.noreply.github.com>
2020-07-30 14:06:49 +0300

    практика функций и методов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,12 @@ import javax.xml.stream.events.Comment;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("данные args= " + args);
-        System.out.println("изменения");
-//        комментарий через Code или ctr+/
-        /*комментарий через Code или ctr+shift+/ -
-        многострочный*/
-//        \u000a System.out.println("бэкдор");
-
-        int age = 3; // сейчас котёнку три года
-        System.out.println(age++); // увеличили на год
-        System.out.println(++age); // выводим результат еще +1 сразу
-
-        char ch=103; // символ 'G'
-        System.out.println(ch);
+    public static void main (String args[]){
+        calcSum(2, 9);
     }
 
-
-
+    static void calcSum (int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+    }
 }
