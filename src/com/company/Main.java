91e6e9b67ebfd8320090a6d2005c15a2d8ec6e1b
Igor-070788 <68988262+Igor-070788@users.noreply.github.com>
2020-07-30 14:11:18 +0300

    методы - закрепление

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,14 @@ import javax.xml.stream.events.Comment;
 public class Main {
 
     public static void main (String args[]){
-        calcSum(2, 9);
+        int Res = calcSum(2, 9);
+        System.out.println(Res);
     }
 
-    static void calcSum (int a, int b) {
+    static int calcSum (int a, int b) {
         int sum = a + b;
-        System.out.println(sum);
+
+        return sum;
+
     }
 }
