a3808bfe638d21f290ecf9106e3fc14d8f45b3da
Igor-070788 <68988262+Igor-070788@users.noreply.github.com>
2020-07-30 19:07:16 +0300

    преобразование строки в число.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,8 @@ package com.company;
 import javax.xml.stream.events.Comment;
 
 public class Main {
-
-    public static void main (String args[]){
-        int Res = calcSum(2, 9);
-        System.out.println(Res);
-    }
-
-    static int calcSum (int a, int b) {
-        int sum = a + b;
-
-        return sum;
+    public static void main (String [] args) {
 
     }
+
 }
