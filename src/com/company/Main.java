a927d0ef95bca5607ebf0c8cfe23b35eb2e61718
Igor-070788 <68988262+Igor-070788@users.noreply.github.com>
2020-07-30 12:15:21 +0300

    изучил типы переменных

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,26 @@
 package com.company;
 
+import javax.xml.stream.events.Comment;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("данные args= " + args);
         System.out.println("изменения");
+//        комментарий через Code или ctr+/
+        /*комментарий через Code или ctr+shift+/ -
+        многострочный*/
+//        \u000a System.out.println("бэкдор");
+
+        int age = 3; // сейчас котёнку три года
+        System.out.println(age++); // увеличили на год
+        System.out.println(++age); // выводим результат еще +1 сразу
+
+        char ch=103; // символ 'G'
+        System.out.println(ch);
     }
+
+
+
 }
