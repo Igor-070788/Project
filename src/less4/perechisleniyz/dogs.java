65d2dbfcc19504f06e3f40d510f12de62b580611
Igor-070788 <68988262+Igor-070788@users.noreply.github.com>
2020-07-31 20:29:19 +0300

    Урок enum 2

--- src/less4/perechisleniyz/dogs.java
+++ src/less4/perechisleniyz/dogs.java
@@ -1,18 +1,29 @@
 package less4.perechisleniyz;
 
 public enum dogs {
-    pit ("питбуль"), dober("доберман"), corgi("корги");
+    pit (45), dober(40), corgi(20);
 
-    private String translation;
+    //private String translation;
+    private int weight;
 
-    dogs(String translation){
+    dogs (int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+ /*   dogs(String translation){
         this.translation = translation;
-
     }
 
     public String getTranslation(){
         return translation;
-
     }
 
+    public String toString(){
+        return "Расшифровка " + translation;
+    }
+*/
 }
--- src/less4/perechisleniyz/less_enum.java
+++ src/less4/perechisleniyz/less_enum.java
@@ -5,8 +5,16 @@ import java.awt.*;
 public class less_enum {
 
     public static void main (String [] args) {
-        dogs dog = dogs.pit;
+/*        dogs dog = dogs.pit;
+        System.out.println(dog.toString());
         System.out.println(dog.getTranslation());
+    }
+    */
+        dogs dog = dogs.pit;
+        dogs dober = dogs.dober;
+        System.out.println(dog.getWeight());
+        System.out.println(dog.name());
+        System.out.println(dober.ordinal());
     }
 /*        dogs dog = dogs.pit;
 
