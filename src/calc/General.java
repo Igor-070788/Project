b13585673479e1701e0bbe12709901d273a67b95
Igor-070788 <68988262+Igor-070788@users.noreply.github.com>
2020-08-18 11:43:00 +0300

    проект

--- src/calc/General.java
+++ src/calc/General.java
@@ -1,23 +1,28 @@
 package calc;
 
-    import java.util.Scanner;
-    import calc.Answer; // подгружаю переменные из класса Answer - ans
+import java.util.Scanner;
+import java.calc.Answer;
+    // подгружаю переменные из класса Answer - ans
 
     public class General {
-                public static void main(String[] args) {
-                    Scanner input = new Scanner(System.in);
-                    Calculator calc = new Calculator();
-                    String myinput;
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            Calculator calc = new Calculator();
+            String myinput;
 
-                    while (true) {
-                        System.out.println("Input: ");
-                        myinput = input.nextLine();
-                    }
-                    if (ans.success)
-                        System.out.println(ans.result);
-                    else {
-                        for (String error: ans.errors)
-                            System.out.println(error);
-                        }
+            while (true) {
+                System.out.println("Input: ");
+                myinput = input.nextLine();
+
+                if (ans.success) {
+                    System.out.println("Output: ");
+                    System.out.println(ans.result);
                 }
+                else {
+                    for (String error : ans.errors)
+                        System.out.println(error);
+                }
+            }
+            input.close();
+        }
     }
